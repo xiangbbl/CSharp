4531a8180903230c1e03fc68c270a0e6312e867c
xiangbbl <55719651+xiangbbl@users.noreply.github.com>
2021-08-17 14:52:49 -0500

    Learn array

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -14,8 +14,12 @@ namespace CSharp
         {
             int i = 0;
             i = con;
-           
-            Console.WriteLine("Hello world{0}", con);
+            i = 999;
+            int[] arr = { 1, 2, 3, 4};
+            for (int j = 0; j < 4; j++) {
+                Console.WriteLine("Hello {0} yes{0}", arr[j], con);
+            }
+            Console.WriteLine($"Hello world{i}.....{con}");
             Console.ReadKey();
         }
     }
