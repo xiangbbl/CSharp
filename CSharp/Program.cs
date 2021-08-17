ce7dddf500e4135f51aababd5943c3f149ca3c61
xiangbbl <55719651+xiangbbl@users.noreply.github.com>
2021-08-17 13:51:49 -0500

    Add project files.

---
+++ CSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    
+    class Program
+    {
+        const int con = 1;
+        static void Main(string[] args)
+        {
+            int i = 0;
+            i = con;
+           
+            Console.WriteLine("Hello world{0}", con);
+            Console.ReadKey();
+        }
+    }
+}
